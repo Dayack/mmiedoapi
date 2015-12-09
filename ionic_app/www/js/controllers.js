d35174d26cc47dcb1c1280533cacaaac54bfaa25
David Verdú <david.verdu@edosoftfactory.com>
2015-12-09 14:37:48 +0000

    Commit with app scaffold made with Ionic Creator

---
+++ ionic_app/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+     
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('mediosCtrl', function($scope) {
+
+})
+   
+.controller('categoriasCtrl', function($scope) {
+
+})
+   
+.controller('noticiasCtrl', function($scope) {
+
+})
+   
+.controller('detalleDeLaNoticiaCtrl', function($scope) {
+
+})
+ 
