21243dd493fcfea73e838fe189fa87213220a91f
robertogonzalezsuarez <roberto.gonzalez@edosoftfactory.com>
2015-12-15 10:05:14 +0000

    node module for getCategories and login (fake)

--- node_server/server/index.js
+++ node_server/server/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var auth = require('../authentication');
 
 module.exports = function() {
 
@@ -10,7 +11,14 @@ module.exports = function() {
 
 	app.get('/graph/:graphname',function(req,res) {
 		res.send('Page for'+req.params.graphname + 'with option' + req.query.option);
-	})
+	});
+
+	app.get('/login/:user/:password',function(req,res) {
+		res.send(auth.login(req.params.user,req.params.password));
+	});
+	app.get('/categories/:user',function(req,res) {
+		res.send(auth.getCategories(req.params.user));
+	});
 
 	//add public static directory
 	app.use(express.static('public'));
