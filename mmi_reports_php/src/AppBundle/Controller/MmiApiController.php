5c4f258755c0d3e6439b257d8aef082b802b3d60
Jessica Garcia <jessica.garcia@edosoftfactory.com>
2015-12-18 11:56:36 +0000

    MmiApi in PHP v3.4

--- mmi_reports_php/src/AppBundle/Controller/MmiApiController.php
+++ mmi_reports_php/src/AppBundle/Controller/MmiApiController.php
@@ -18,8 +18,8 @@ class MmiApiController extends Controller
      */
     public function info_page(Request $request)
     {
-        //http://localhost:8000/info_page?idzona=1&idusuario=1344&idvista=1
-        $request_data = array('idusuario','idzona','idvista');
+        //http://localhost:8000/info_page?id_zona=1&id_usuario=1344&id_informe=1
+        $request_data = array('id_usuario','id_zona','id_informe');
         foreach ($request_data as $request_value) {
             if (strlen($request->query->get($request_value)) > 0) {
                 $$request_value = $request->query->get($request_value);
@@ -31,18 +31,17 @@ class MmiApiController extends Controller
         
         $base_url = 'http://api.mmi-e.com/mmiapi.php/';
         $apikey = 'DFKGMKLJOIRJNG';
-        $getusuarios_perfil = $base_url.'getusuarios_perfil/'.$apikey.'/'.$idzona.'/'.$idusuario;
+        $getusuarios_perfil = $base_url.'getusuarios_perfil/'.$apikey.'/'.$id_zona.'/'.$id_usuario;
             //$response_decode[0]->{'IDPERFIL'}
-        $getperfiles_categorias = $base_url.'getperfiles_categorias/'.$apikey.'/'.$idzona.'/';
-        $getcategoria_detalle = $base_url.'getcategoria_detalle/'.$apikey.'/'.$idzona.'/';
+        $getperfiles_categorias = $base_url.'getperfiles_categorias/'.$apikey.'/'.$id_zona.'/';
+        $getcategoria_detalle = $base_url.'getcategoria_detalle/'.$apikey.'/'.$id_zona.'/';
             //$response_decode[0]->{'NOMBREWEB'}
-        $getvistanombres = $base_url.'getvistanombres/'.$apikey.'/'.$idzona.'/'.$idusuario;
+        $getvistanombres = $base_url.'getvistanombres/'.$apikey.'/'.$id_zona.'/'.$id_usuario;
         
         $usuario_categorias_id = array();
         $usuario_categorias_nombre = array();
-        $categorias_usuario = '';
         $views = '';
-        if ($idzona > 0 && $idusuario > 0) {
+        if ($id_zona > 0 && $id_usuario > 0) {
             $restClient = new RestClient(new Curl(new CurlOptionsHandler(array())));
             
     // inicio conseguir idperfil
@@ -73,7 +72,6 @@ class MmiApiController extends Controller
     // fin conseguir nombre categorias
                 }
     // inicio conseguir datos vistas
-                $views = '';
 /*
                 $response = $restClient->get($getvistanombres);
                 $response_decode = json_decode($response->getContent());
@@ -81,14 +79,14 @@ class MmiApiController extends Controller
                 }
 */
                 $views = '{"id_informe": 1, "nombre_informe": "Vista de cubrimiento 1", "tipo_informe" : "grafica1"}, {"id_informe": 2, "nombre_informe": "vista de cubrimiento 2", "tipo_informe": "grafica2"}';
-                
+    // fin conseguir datos vistas
             }
         }
 
         return $this->render('mmiapi/template1.html.twig', array(
-            'idusuario' => $idusuario,
-            'idzona' => $idzona,
-            'idvista' => $idvista,
+            'id_usuario' => $id_usuario,
+            'id_zona' => $id_zona,
+            'id_informe' => $id_informe,
 
             'nombre_categorias' => '"'.implode('","', $usuario_categorias_nombre).'"',
             'id_categorias' => implode(",", $usuario_categorias_id),
@@ -103,8 +101,8 @@ class MmiApiController extends Controller
      */
     public function manage_page(Request $request)
     {
-        //http://localhost:8000/manage_page?idzona=1&idusuario=1344&idvista=1
-        $request_data = array('idusuario','idzona','idvista');
+        //http://localhost:8000/manage_page?id_zona=1&id_usuario=1344&id_informe=1
+        $request_data = array('id_usuario','id_zona','id_informe');
         foreach ($request_data as $request_value) {
             if (strlen($request->query->get($request_value)) > 0) {
                 $$request_value = $request->query->get($request_value);
@@ -116,23 +114,73 @@ class MmiApiController extends Controller
 
         $base_url = 'http://api.mmi-e.com/mmiapi.php/';
         $apikey = 'DFKGMKLJOIRJNG';
+        $getusuarios_perfil = $base_url.'getusuarios_perfil/'.$apikey.'/'.$id_zona.'/'.$id_usuario;
+            //$response_decode[0]->{'IDPERFIL'}
+        $getperfiles_categorias = $base_url.'getperfiles_categorias/'.$apikey.'/'.$id_zona.'/';
+        $getcategoria_detalle = $base_url.'getcategoria_detalle/'.$apikey.'/'.$id_zona.'/';
+            //$response_decode[0]->{'NOMBREWEB'}
+        $getinforme_grupos = $base_url.'getinforme_grupos/'.$apikey.'/'.$id_zona.'/'.$id_informe;
+
+        $usuario_categorias_id = array();
+        $usuario_categorias_nombre = array();
+        $view = '';
+        if ($id_zona > 0 && $id_usuario > 0) {
+            $restClient = new RestClient(new Curl(new CurlOptionsHandler(array())));
+
+    // inicio conseguir idperfil
+            $response = $restClient->get($getusuarios_perfil);
+            $response_decode = json_decode($response->getContent());
+            if (gettype($response_decode[0]) != 'boolean') {
+                $idperfil = $response_decode[0]->{'IDPERFIL'};
+    // fin conseguir idperfil
+
+    // inicio conseguir id categorias
+                $response = $restClient->get($getperfiles_categorias . $idperfil);
+                $response_decode = json_decode($response->getContent());
+                if (count($response_decode) > 0) {
+                    foreach ($response_decode as $value_category) {
+                        if (!in_array($value_category->{'IDCATEGORIA'}, $usuario_categorias_id)) {
+                            $usuario_categorias_id[] = $value_category->{'IDCATEGORIA'};
+                        }
+                    }
+                    sort($usuario_categorias_id);
+    // fin conseguir id categorias
+
+    // inicio conseguir nombre categorias
+                    foreach ($usuario_categorias_id as $categoria_id) {
+                        $response = $restClient->get($getcategoria_detalle . $categoria_id);
+                        $response_decode = json_decode($response->getContent());
+                        $usuario_categorias_nombre[] = $response_decode[0]->{'NOMBREWEB'};
+                    }
+    // fin conseguir nombre categorias
+
+    // inicio conseguir datos vista
+/*
+                    if ($id_informe > 0) {
+                        $response = $restClient->get($getinforme_grupos);
+                        $response_decode = json_decode($response->getContent());
+                        if (count($response_decode)> 0) {
+                        }
+                    }
+*/
+                    $view = '{"tipo_informe": "Grafica_barras1",
+"nombre_informe": "TOTAL DE NOTICIAS POR CUBRIMIENTO",
+"grupo_de_categorias": [{ "nombre_grupo_de_categorias": "Regional", "categorias": [2,3,4]},
+{ "nombre_grupo_de_categorias": "Nacional", "categorias": [5,6,7]}]}';
+    // fin conseguir datos vista
+                }
+            }
+        }
+
         return $this->render('mmiapi/template2.html.twig', array(
-            'idusuario' => $idusuario,
-            'idzona' => $idzona,
-            'view' => 0,
-            'nombre_categorias' => 0,
-            'id_categorias' => 0,
+            'id_usuario' => $id_usuario,
+            'id_zona' => $id_zona,
+            'id_informe' => $id_informe,
+
+            'view' => $view,
+            'nombre_categorias' => '"'.implode('","', $usuario_categorias_nombre).'"',
+            'id_categorias' => implode(",", $usuario_categorias_id),
         ));
-        /*
-                    'idusuario' => $idusuario,
-                    'idzona' => $idzona,
-
-                    'nombres_categorias' => implode('","', $usuario_categorias_nombre),
-                    'ids_categorias' => implode(",", $usuario_categorias_id),
-                    'views' => $views,
-
-        */
-
     }
 
     /**
@@ -140,6 +188,19 @@ class MmiApiController extends Controller
      */
     public function graph_page(Request $request)
     {
+        //http://localhost:8000/graph_page?id_zona=1&id_usuario=1344&id_informe=1&desde=01092014&hasta=02092014&id_categoria=24
+        $request_data = array('id_usuario','id_zona','id_informe','desde','hasta','id_categoria');
+        foreach ($request_data as $request_value) {
+            if (strlen($request->query->get($request_value)) > 0) {
+                $$request_value = $request->query->get($request_value);
+            }
+            else {
+                $$request_value = 0;
+            }
+        }
+
+        $base_url = 'http://api.mmi-e.com/mmiapi.php/';
+        $apikey = 'DFKGMKLJOIRJNG';
         return $this->render('mmiapi/template3.html.twig', array(
             'idusuario' => 0,
             'idzona' => 0,
