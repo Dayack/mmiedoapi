13ded72ea7c254c977f525e937b904d999a4f11d
robertogonzalezsuarez <roberto.gonzalez@edosoftfactory.com>
2016-02-24 17:48:41 +0000

    loading detail

--- ionic_app/www/js/controllers.js
+++ ionic_app/www/js/controllers.js
@@ -640,7 +640,7 @@ angular.module('app.controllers', [])
     };
 })
 
-.controller('detalleCtrl', function(ScrollService,$location,$rootScope,$sce,$http,$timeout,$document,$ionicHistory,$cordovaInAppBrowser,$scope,$window,UserService,CategoryService,$state,$ionicNavBarDelegate,$stateParams,NewsService) {
+.controller('detalleCtrl', function($ionicLoading,ScrollService,$location,$rootScope,$sce,$http,$timeout,$document,$ionicHistory,$cordovaInAppBrowser,$scope,$window,UserService,CategoryService,$state,$ionicNavBarDelegate,$stateParams,NewsService) {
 
     $scope.media= $stateParams.media;
     $scope.date= $stateParams.date;
